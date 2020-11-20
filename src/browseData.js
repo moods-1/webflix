5bca4ab958918407f3e1d2b89daf12b9ec697f9c
moods-1 <66252073+moods-1@users.noreply.github.com>
2020-11-20 08:41:30 -0500

    Nav redesign.

---
+++ src/browseData.js
@@ -0,0 +1,8 @@
+export const browseData = {
+    ulMain: ["Home","My List","Originals","New Arrivals","Audio & Subtitles","Ways to Watch"],
+    ulLeft: ["TV","Action","Award-Winning","Comedies","Crime Films"],
+    ulCenter: ["Documentaries","Dramas","Horror","Independent","Kids & family"],
+    ulRight: ["International","Music & Musicals","Romance","Sci-Fi","Sports"]
+}
+
+
