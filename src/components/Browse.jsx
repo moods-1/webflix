35085abfbf700ca9f9830e1327127582efb5c9ab
moods-1 <ceemoods@gmail.com>
2021-03-24 10:15:11 -0400

    Mouse leave functionality added to browse menu.

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
-import {browseData} from '../browseData';
+import React from "react";
+import { browseData } from "../browseData";
 
-function Browse({setShowBrowse}) {
-       
-    return (
-        <div id="browse-menu">
-            <div id="browse-left-box">
-                <ul id="browse-ul-main">
-                    {browseData.ulMain.map(link=>(
-                       <li  key={link} onClick={()=> setShowBrowse(false)}>{link}</li> 
-                    ))}
-                </ul>
-            </div>
-            <div id="browse-right-box">
-                <ul id="browse-ul-left">
-                    {browseData.ulLeft.map(link=>(
-                        <li  key={link} onClick={()=> setShowBrowse(false)}>{link}</li> 
-                        ))}
-                </ul>
-                <ul id="browse-ul-center">
-                    {browseData.ulCenter.map(link=>(
-                        <li  key={link} onClick={()=> setShowBrowse(false)}>{link}</li> 
-                        ))}
-                </ul>
-                <ul id="browse-ul-right">
-                    {browseData.ulRight.map(link=>(
-                        <li  key={link} onClick={()=> setShowBrowse(false)}>{link}</li> 
-                        ))}
-                </ul>
-            </div>
-        </div>  
-    )
+function Browse({ setShowBrowse }) {
+  return (
+    <div id="browse-menu" onMouseLeave={() => setShowBrowse(false)}>
+      <div id="browse-left-box">
+        <ul id="browse-ul-main">
+          {browseData.ulMain.map((link) => (
+            <li key={link} onClick={() => setShowBrowse(false)}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div id="browse-right-box">
+        <ul id="browse-ul-left">
+          {browseData.ulLeft.map((link) => (
+            <li key={link} onClick={() => setShowBrowse(false)}>
+              {link}
+            </li>
+          ))}
+        </ul>
+        <ul id="browse-ul-center">
+          {browseData.ulCenter.map((link) => (
+            <li key={link} onClick={() => setShowBrowse(false)}>
+              {link}
+            </li>
+          ))}
+        </ul>
+        <ul id="browse-ul-right">
+          {browseData.ulRight.map((link) => (
+            <li key={link} onClick={() => setShowBrowse(false)}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 export default Browse;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,110 +1,87 @@
-import React, {useState,useEffect} from 'react';
-import PersonIcon from '@material-ui/icons/Person';
-import SearchIcon from '@material-ui/icons/Search';
-import Browse from '../components/Browse';
-import '../styles/Nav.css';
-
-let arrowSpin = false;
+import React, { useState, useEffect } from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import SearchIcon from "@material-ui/icons/Search";
+import Browse from "../components/Browse";
+import "../styles/Nav.css";
 
 function Nav() {
-    const [show, setShow] = useState(false);
-    const [showBrowse, setShowBrowse] = useState(false);
-    const [burgerMenu, setBurgerMenu] = useState(false);
-    
-    useEffect(()=>{
-        window.addEventListener("scroll",()=> {
-            setShow(window.scrollY > 25? true:false);
-            setBurgerMenu(false); 
-        })
-        return ()=> window.removeEventListener("scroll",()=> true)
-    },[]);
-    
-    useEffect(()=>{
-        window.addEventListener("resize",()=> {            
-            window.innerWidth > 510 && setBurgerMenu(false)
-        })
-        return ()=> window.removeEventListener("resize",()=> true)
-    },[]);
-    
-    const browseToggle = e =>{
-        arrowSpin = !arrowSpin;
-        e.target.style.animationName = arrowSpin? "redSpin":"";
-        setShowBrowse(!showBrowse);
-    }
-    const handleBurger = e =>{
-        setBurgerMenu(!burgerMenu);
-        setShowBrowse(false);
-    }
-    const handleBurgerBrowse = e =>{
-        setShowBrowse(!showBrowse);
-        setBurgerMenu(!burgerMenu);
-    }
+  const [show, setShow] = useState(false);
+  const [showBrowse, setShowBrowse] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
 
-    return (
-        <div className={`nav ${show && "nav-black"}`}>
-            <div id="logo-browse-box">
-                <img 
-                    className="nav-logo"
-                    src='/images/webflix.png'
-                    alt='webflix-logo'
-                /> 
-                <div id="browse-arrow" >
-                    <h4 style={{color:"red"}}>Browse</h4>
-                    <img 
-                        src="/images/redDownArrow.png" 
-                        alt="red down arrow"
-                        id="red-arrow"
-                        onClick={browseToggle}
-                    />
-                </div> 
-                {showBrowse && <Browse  setShowBrowse={setShowBrowse}/>}
-            </div>
-            <div id="nav-right-box">
-                <div id="search-container">                    
-                    <SearchIcon 
-                        style={{ 
-                            marginRight: "5px",
-                            cursor: "pointer" 
-                        }}
-                    />
-                    <input 
-                        type="text" 
-                        id="search-box"
-                        placeholder="Search"
-                     />                
-                </div>
-                <div id="user-box">
-                    <img 
-                        className="bell"
-                        src='/images/bell.png'
-                        alt='bell'
-                    />
-                    <PersonIcon 
-                        style={{
-                            fontSize: 30,
-                            color: "red",
-                            cursor: "pointer"
-                        }}
-                    />
-                </div>    
-            </div> 
-            <img 
-                src="/images/redBurger.png" 
-                alt="burger"
-                id="burger"
-                onClick={handleBurger}
-            />
-            {burgerMenu && (
-                <div id="burger-menu-box">
-                    <ul>
-                        <li onClick={handleBurgerBrowse}>Browse</li>
-                        <li onClick={handleBurger}>Search</li>
-                        <li onClick={handleBurger}>Notifications</li>
-                        <li onClick={handleBurger}>Logout</li>
-                    </ul>
-                </div>
-            )}     
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setShow(window.scrollY > 25 ? true : false);
+      setBurgerMenu(false);
+    });
+    return () => window.removeEventListener("scroll", () => true);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      window.innerWidth > 510 && setBurgerMenu(false);
+    });
+    return () => window.removeEventListener("resize", () => true);
+  }, []);
+
+  const handleBurger = (e) => {
+    setBurgerMenu(!burgerMenu);
+    setShowBrowse(false);
+  };
+  const handleBurgerBrowse = (e) => {
+    setShowBrowse(!showBrowse);
+    setBurgerMenu(!burgerMenu);
+  };
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <div id="logo-browse-box">
+        <img
+          className="nav-logo"
+          src="/images/webflix.png"
+          alt="webflix-logo"
+        />
+        <h4 onClick={()=> setShowBrowse(!showBrowse)}>Browse</h4>
+        {showBrowse && <Browse setShowBrowse={setShowBrowse} />}
+      </div>
+      <div id="nav-right-box">
+        <div id="search-container">
+          <SearchIcon
+            style={{
+              marginRight: "5px",
+              cursor: "pointer",
+            }}
+          />
+          <input type="text" id="search-box" placeholder="Search" />
         </div>
-    )
+        <div id="user-box">
+          <img className="bell" src="/images/bell.png" alt="bell" />
+          <PersonIcon
+            style={{
+              fontSize: 30,
+              color: "red",
+              cursor: "pointer",
+            }}
+          />
+        </div>
+      </div>
+      <img
+        src="/images/redBurger.png"
+        alt="burger"
+        id="burger"
+        onClick={handleBurger}
+      />
+      {burgerMenu && (
+        <div id="burger-menu-box">
+          <ul>
+            <li onClick={handleBurgerBrowse}>Browse</li>
+            <li onClick={handleBurger}>Search</li>
+            <li onClick={handleBurger}>Notifications</li>
+            <li onClick={handleBurger}>Logout</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
-export default Nav
+export default Nav;
